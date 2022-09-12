3944c138815bb8e044552efc2f82cb083bb81fc1
filipapico <69467046+filipapico@users.noreply.github.com>
2022-09-12 10:47:55 +0100

    Created game grid structure in memory_game.html, including flex structure and Css flex styling

---
+++ MemoryGame/memory_game.js
@@ -0,0 +1,9 @@
+//let cards = [<i className="fa-solid fa-dog"></i>,<i className="fa-solid fa-cat"></i>]
+
+/*CARDS
+DOG <i class="fa-solid fa-dog"></i>
+CAT <i className="fa-solid fa-cat"></i>
+
+Other animals https://fontawesome.com/icons/categories/animals
+Other icons https://www.w3schools.com/icons/
+ */
