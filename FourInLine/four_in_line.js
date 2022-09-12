a24af63c12ddb91f51535e21fef3fac28e402ad8
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-09-12 23:36:42 +0100

    fourInLine: created function colorBottomCell to color the last empty cell available for each column

--- FourInLine/four_in_line.js
+++ FourInLine/four_in_line.js
@@ -1,9 +1,46 @@
-let cellDiv = document.querySelector(".div-cell");
+let column = document.querySelector(".div-col");
 
-function getColored(event){
-    cellDiv.style.backgroundColor = "red";
+//Create array of column cells and give them an initial state (eg black)
+let newArray = [];
+let id = "#";
+for (let i = 0; i <= 5; i++) {
+    newArray[i] = document.querySelector(id + "A" + i);
+    newArray[i].style.backgroundColor = "black";
 }
-cellDiv.addEventListener("click", getColored);
+console.log(newArray);
 
-console.log("hello");
 
+//Create arrays for the grid:
+
+let alphArray = ["A", "B", "C", "D", "E", "F", "G"];
+let numArray = [ 0, 1, 2, 3, 4, 5];
+
+let gridArrays = alphArray.map(function(letter){
+    let arrayLetter = [];
+    for(let i = 0; i < numArray.length; i++){
+        arrayLetter[i] = letter + numArray[i];
+    }
+    console.log(arrayLetter);
+    return arrayLetter;
+})
+
+console.log(gridArrays);
+
+//on click color the last empty bottom cell of each column
+
+let i = newArray.length - 1;
+let currentEmpty = newArray[i];
+let newEmpty;
+let colorRed = "red";
+
+function colorBottomCell(e) {
+    if (currentEmpty.style.backgroundColor === colorRed) {
+        newEmpty = newArray[i--];
+        newEmpty.style.backgroundColor = colorRed;
+        currentEmpty = newEmpty;
+    } else {
+        currentEmpty.style.backgroundColor = colorRed;
+    }
+}
+
+column.addEventListener("click", colorBottomCell);
