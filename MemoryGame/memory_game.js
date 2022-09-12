32b46e1a4df8f4861bf13fd69d0d0370e2d4768e
filipapico <69467046+filipapico@users.noreply.github.com>
2022-09-12 15:52:05 +0100

    1. Added (memory_game.html) the fontawesome script 2. Copied (to memory_game.js) the list of selected fontawesome icons (some still saved as commented text) 3. Added (to memory_game.js) a function shuffleArray (copied a solution form the www) to get a new shuffled array of "cards" before the memory game starts.

--- MemoryGame/memory_game.js
+++ MemoryGame/memory_game.js
@@ -1,8 +1,57 @@
-//let cards = [<i className="fa-solid fa-dog"></i>,<i className="fa-solid fa-cat"></i>]
+
+let card = document.querySelector("#position0-0");
+let cards = ["fa-dog","fa-cat","fa-hippo","fa-horse"]
+
+function shuffleArray(array) {
+    let curId = array.length;
+    // There remain elements to shuffle
+    while (0 !== curId) {
+        // Pick a remaining element
+        let randId = Math.floor(Math.random() * curId);
+        curId -= 1;
+        // Swap it with the current element.
+        let tmp = array[curId];
+        array[curId] = array[randId];
+        array[randId] = tmp;
+    }
+    return array;
+}
+// Usage of shuffle
+shuffleArray(cards);
+console.log(cards);
+
+card.addEventListener("click", function (e){
+    card.innerHTML =
+        `<i class="fa-solid ${shuffleArray(cards[0])}"></i>`
+})
+
+// FP TESTE
+/*function getRandomList (list){
+    let gameGrid = [];
+    for (let i=0; i<list.length; i++) {
+        let randomCard = Math.floor(Math.random()*list.length);
+        console.log(randomCard)
+        gameGrid[i] = list[randomCard]
+    }
+    return gameGrid;
+}
+
+console.log(getRandomList(cards))*/
 
 /*CARDS
-DOG <i class="fa-solid fa-dog"></i>
-CAT <i className="fa-solid fa-cat"></i>
+<i class="fa-solid fa-dog"></i>
+<i class="fa-solid fa-cat"></i>
+<i class="fa-light fa-hippo"></i>
+<i class="fa-light fa-horse"></i>
+<i class="fa-solid fa-fish"></i>
+<i class="fa-solid fa-turtle"></i>
+<i class="fa-light fa-whale"></i>
+<i class="fa-light fa-bat"></i>
+<i class="fa-solid fa-rabbit"></i>
+<i class="fa-light fa-duck"></i>
+
+
+
 
 Other animals https://fontawesome.com/icons/categories/animals
 Other icons https://www.w3schools.com/icons/
