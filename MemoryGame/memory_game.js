8ed94306ae89b4c974990d13a6274b4aac555ed1
filipapico <69467046+filipapico@users.noreply.github.com>
2022-09-14 14:54:22 +0100

    1. Created new array of "cards" bacause I couldn't load the fontawesome set of imagens and the remaining code was not running (in memory_game.js) 2. Because of 1 had to rewrite parts of the code, commented in the memory_game.js file 3. Created function the check if 2 cards are equal or not (in memory_game.js) 4. Changed css classes to see the changes in game defined in the previous number. PS. More changes than usual in my commits because I had no internet connection until now

--- MemoryGame/memory_game.js
+++ MemoryGame/memory_game.js
@@ -1,12 +1,18 @@
 let card = document.querySelectorAll(".row-item");
-console.log("Individual card position item", card)
+//console.log("Individual card position item", card)
 
 // All the available cards
-let cards = ["dog", "dog", "cat", "cat", "hippo", "hippo", "horse", "horse", "frog", "frog", "fish", "fish", "spider", "spider", "kiwi-bird", "kiwi-bird", "feather-pointed", "feather-pointed", "mosquito", "mosquito"]
+// CARDS from fontawesome - ONLY WORKS WITH INTERNET
+// let cards = ["dog", "dog", "cat", "cat", "hippo", "hippo", "horse", "horse", "frog", "frog", "fish", "fish", "spider", "spider", "kiwi-bird", "kiwi-bird", "feather-pointed", "feather-pointed", "mosquito", "mosquito"]
+// WITHOUT INTERNET
+let cards = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10"]
+
+// GAME - Create grid
+//MISSING PART
 
 // GAME - Grid easy (3 x 4 = 12)
 let cardsEasy = cards.slice(0, 12)
-console.log("All cards - Grid easy(3x4)", cardsEasy)
+//console.log("All cards - Grid easy(3x4)", cardsEasy)
 
 // Function to shuffle obtained in the www
 function shuffleArray(array) {
@@ -24,33 +30,69 @@ function shuffleArray(array) {
     return array;
 }
 
+// Function to create game grid with shuffled cards (using previous shuffle array function)
 function setBoard(array) {
-    let cards = shuffleArray(array)
-    let board = document.querySelectorAll(".row-item");
-    console.log("Game grid with shuffled cards", board);
-    for (let i = 0; i < cards.length; i++) {
-        board[i].innerHTML = `<i class="fa fa-${cards[i]} fa-4x"></i>`
+    let shuffledCards = shuffleArray(array)
+    let allCards = document.querySelectorAll(".row-item");
+    //console.log("Game grid with all shuffled shuffledCards", allCards);
+    for (let i = 0; i < shuffledCards.length; i++) {
+        // ONLY WORKS WITH INTERNET
+        //allCards[i].innerHTML = `<i class="fa fa-${shuffledCards[i]} fa-4x"></i>`
+        //WITHOUT INTERNET
+        allCards[i].innerHTML = `<span>${shuffledCards[i]}</span>`
     }
 }
 
 setBoard(cardsEasy)
 
-let newBoard = document.querySelectorAll(".row-item");
-console.log(newBoard[0].innerHTML)
-
+// Function to show each card each time it is clicked
 function showEachCard() {
-    let showCard = document.querySelectorAll(".row-item")
-    console.log(showCard)
-    for (let i = 0; i < showCard.length; i++) {
-        showCard[i].addEventListener("click", function (e) {
-            console.log(showCard[i])
-            showCard[i].classList.toggle("test")
+    //ONLY WORK WITH INTERNET
+    //let allCardsElements = document.querySelectorAll(".row-item >i")
+    // WITHOUT INTERNET
+    let allCardsElements = document.querySelectorAll(".row-item")
+    //WITH INTERNET
+    // console.log("allCards_i elements", allCardsElements)
+    //WITHOUT INTERNET
+    console.log("allCards elements", allCardsElements)
+    console.log(allCardsElements.length)
+    for (let i = 0; i < allCardsElements.length; i++) {
+        allCardsElements[i].addEventListener("click", function (e) {
+            //console.log("clicked card html", allCardsElements[i])
+            allCardsElements[i].classList.toggle("visible")
+            let visibleCards = document.querySelectorAll(".visible")
+            //console.log("visible cards", visibleCards)
+            // NOT WORKING YET - Call the function that checks if clicked cards are equal
+            checkEqualCards(visibleCards)
         })
     }
 }
 
 showEachCard()
 
+//WITHOUT INTERNET
+function checkEqualCards(array) {
+    console.log(array.length)
+    console.log(array[0].innerHTML)
+    console.log(array[1].innerHTML)
+    setTimeout(function () {
+        if (array[0].innerHTML !== array[1].innerHTML) {
+            array[0].classList.remove("visible");
+            array[1].classList.remove("visible")
+        } else{
+            array[0].classList.add("solved");
+            array[1].classList.add("solved");
+            setTimeout(function (){
+                array[0].classList.remove("visible");
+                array[1].classList.remove("visible");
+            },1000)
+            console.log(array)
+        }
+    },1500)
+}
+
+// ______________________________________
+// ______________________________________
 
 /*CARDS - Free
 <i class="fa-solid fa-dog"></i>
