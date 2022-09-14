51fa593decf66992e89b764933c365dc58e6624c
filipapico <69467046+filipapico@users.noreply.github.com>
2022-09-14 16:39:04 +0100

    Removed variable that was a repetition of "allCards" (memory_game.js)

--- MemoryGame/memory_game.js
+++ MemoryGame/memory_game.js
@@ -1,9 +1,7 @@
-let card = document.querySelectorAll(".row-item");
-
 // All the available cards
 //let cards = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10"]
-//POSSIBLE NEW ICONS
 let cards = ["☀️", "☀️", "🐻", "🐻", "👩‍", "👩‍", "🌍", "🌍", "🌻", "🌻", "🐶", "🐶", "😸", "😸", "🦴", "🦴", "🐸", "🐸", "🐣", "🐣"]
+let allCards = document.querySelectorAll(".row-item");
 
 // GAME - Create grid
 //MISSING PART
@@ -15,13 +13,10 @@ let cardsEasy = cards.slice(0, 12)
 // Function to shuffle obtained in the www
 function shuffleArray(array) {
     let curId = array.length;
-    // There remain elements to shuffle
-    while (0 !== curId) {
-        // Pick a remaining element
-        let randId = Math.floor(Math.random() * curId);
+    while (0 !== curId) { // There remain elements to shuffle
+        let randId = Math.floor(Math.random() * curId); // Pick a remaining element
         curId -= 1;
-        // Swap it with the current element.
-        let tmp = array[curId];
+        let tmp = array[curId]; // Swap it with the current element.
         array[curId] = array[randId];
         array[randId] = tmp;
     }
@@ -31,7 +26,6 @@ function shuffleArray(array) {
 // Function to create game grid with shuffled cards (using previous shuffle array function)
 function setBoard(array) {
     let shuffledCards = shuffleArray(array)
-    let allCards = document.querySelectorAll(".row-item");
     //console.log("Game grid with all shuffled shuffledCards", allCards);
     for (let i = 0; i < shuffledCards.length; i++) {
         allCards[i].innerHTML = `<span>${shuffledCards[i]}</span>`
@@ -41,42 +35,39 @@ function setBoard(array) {
 setBoard(cardsEasy)
 
 // Function to show each card each time it is clicked
+let solvedCards = []
 function showEachCard() {
-    let allCardsElements = document.querySelectorAll(".row-item") //THE SAME AS IN LINE 36?
-    console.log("allCards elements", allCardsElements)
-    for (let i = 0; i < allCardsElements.length; i++) {
-        allCardsElements[i].addEventListener("click", function (e) {
-            allCardsElements[i].classList.toggle("visible")
-            let visibleCards = document.querySelectorAll(".visible")
-            checkEqualCards(visibleCards)
+    for (let i = 0; i < allCards.length; i++) {
+        allCards[i].addEventListener("click", function (e) {
+            if (solvedCards.length === 12) {
+                console.log("GANHOU!")
+            } else {
+                allCards[i].classList.toggle("visible")
+                let visibleCards = document.querySelectorAll(".visible")
+                checkEqualCards(visibleCards)
+            }
         })
     }
 }
 
 showEachCard()
 
-let solvedCards = []
-
 function checkEqualCards(array) {
-    if (solvedCards.length >= 11) {
-        console.log("GANHOU!")
-    } else {
-        setTimeout(function () {
-            if (array[0].innerHTML !== array[1].innerHTML) {
+    setTimeout(function () {
+        if (array[0].innerHTML !== array[1].innerHTML) {
+            array[0].classList.remove("visible");
+            array[1].classList.remove("visible")
+        } else {
+            array[0].classList.add("solved");
+            array[1].classList.add("solved");
+            //console.log("solved", array)
+            solvedCards.push(array[0])
+            solvedCards.push(array[1])
+            //console.log("solvedcards", solvedCards)
+            setTimeout(function () {
                 array[0].classList.remove("visible");
-                array[1].classList.remove("visible")
-            } else {
-                array[0].classList.add("solved");
-                array[1].classList.add("solved");
-                console.log("solved", array)
-                solvedCards.push(array[0])
-                solvedCards.push(array[1])
-                console.log("solvedcards", solvedCards)
-                setTimeout(function () {
-                    array[0].classList.remove("visible");
-                    array[1].classList.remove("visible");
+                array[1].classList.remove("visible");
                 }, 1000)
             }
         }, 1500)
     }
-}
